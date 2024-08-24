e9e333baa37a09fa0876fa8f704f43a4914600e2
nikoatto <nikojikkyou@yahoo.co.jp>
2024-08-24 09:35:09 +0900

    デザイン作成したのでサイト作成開始。TOPページを作成中

--- app/page.js
+++ app/page.js
@@ -6,29 +6,46 @@ import Link from 'next/link';
 export default function Home() {
   return (
     <>
-    <header>
-      <h1 className={commonStyles.title}>TAKANASHI NIKO</h1>
-      <nav>
-        <ul>
-          <li>
-            <Link href="/">
-              HOME
-            </Link>
-          </li>
-          <li>
-            <Link href="/about">
-              ABOUT
-            </Link>
-          </li>
-          <li>
-            <Link href="/contact">
-              CONTACT
-            </Link>
-          </li>
-        </ul>
-      </nav>
-    </header>
-    <Image src="/takanashi_logo.svg" alt="小鳥遊にこのlogo" width={500} height={500} />
+      <header>
+        <div className="headerContainer">
+          <Link href="/">
+            <Image src="takanashi_logo_type.svg" width={200} height={80} />
+          </Link>
+          <nav>
+            <ul>
+              <li>
+                <Link href="/">
+                  HOME
+                </Link>
+              </li>
+              <li>
+                <Link href="/about">
+                  ABOUT
+                </Link>
+              </li>
+              <li>
+                <Link href="/contact">
+                  CONTACT
+                </Link>
+              </li>
+            </ul>
+          </nav>
+        </div>
+      </header>
+      <div className="mainContainer">
+        <div className="topContainer">
+          <Image className="topLogo" src="/takanashi_logo.svg" alt="小鳥遊にこのlogo" width={500} height={500} />
+        </div>
+        <div className="topWorkContainer">
+          <div className="topMainInnner">
+            <div className="topWorkHead">
+              <h2 className="topMainCaption">WORK</h2>
+              <p className="topMainDesc">私の描いたイラストです。ボヤけてるのはセンシティブなやつ。えっちな絵も描きます。</p>
+            </div>
+          </div>
+        </div>
+      </div>
+
     </>
   );
 }
